755338d20115bd6ab8285dfca0b763a63ebb016c
Kaspar Emanuel <kaspar@kitspace.org>
2025-05-23 20:15:06 +0100

    Load in wasm

--- rust-pattern-search/src/lib.rs
+++ rust-pattern-search/src/lib.rs
@@ -7,9 +7,10 @@ pub mod errors;
 pub mod sgf_traversal;
 
 use cfg_if::cfg_if;
+use rmp_serde::{Deserializer, Serializer};
+use serde::{Deserialize, Serialize};
 use sgf_parse::{go, go::Point, SgfNode};
 use std::collections::HashMap;
-use std::path::Path;
 use wasm_bindgen::prelude::*;
 
 cfg_if! {
@@ -23,104 +24,127 @@ cfg_if! {
 #[wasm_bindgen]
 extern "C" {
     fn alert(s: &str);
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
 }
 
 #[wasm_bindgen]
 pub fn greet(name: &str) {
-    alert(&format!("Hello,{}!", name));
+    log("greet");
+    let data = include_bytes!("games.pack");
+    let mut de = Deserializer::new(&data[..]);
+    let games: HashMap<String, Vec<Move>> = Deserialize::deserialize(&mut de).unwrap();
+    log("Loaded games");
+    let point = Point { x: 3, y: 3 };
+    let color = Color::White;
+    for (path, moves) in games {
+        for move_ in moves {
+            if move_.point == point && move_.color == color {
+                break;
+            }
+        }
+    }
+    log("x");
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub enum Color {
     Black,
     White,
 }
 
-type BoardPosition = HashMap<Point, Color>;
+#[derive(Serialize, Deserialize)]
+#[serde(remote = "Point")]
+struct PointDef {
+    x: u8,
+    y: u8,
+}
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct Move {
     color: Color,
+    #[serde(with = "PointDef")]
     point: Point,
 }
 
-pub fn point_in_game(sgf_nodes: Vec<SgfNode<go::Prop>>, point: Point) -> bool {
-    false
-}
-
-pub fn position_in_game(sgf_nodes: Vec<SgfNode<go::Prop>>, position: BoardPosition) -> bool {
-    false
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    fn load_sgfs() -> Result<HashMap<String, Vec<Move>>, Box<dyn std::error::Error>> {
-        let mut sgf_folder = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-        sgf_folder.push("badukmovies-pro-collection");
-
-        let mut games = HashMap::new();
-
-        for entry in jwalk::WalkDir::new(sgf_folder) {
-            let path = entry?.path();
-            if path.extension().map_or(false, |ext| ext == "sgf") {
-                let file_data = std::fs::read_to_string(path.clone())?;
-                if let Ok(game) = go::parse(&file_data) {
-                    let mut moves = Vec::new();
-                    for node in sgf_traversal::variation_nodes(&game[0], 0).unwrap() {
-                        for props in &node.sgf_node.properties {
-                            match props {
-                                go::Prop::W(go::Move::Move(point)) => {
-                                    moves.push(Move {
-                                        color: Color::White,
-                                        point: point.clone(),
-                                    });
-                                    break;
-                                }
-                                go::Prop::B(go::Move::Move(point)) => {
-                                    moves.push(Move {
-                                        color: Color::Black,
-                                        point: point.clone(),
-                                    });
-                                    break;
-                                }
-                                _ => {}
-                            }
-                        }
-                    }
-
-                    games.insert(path.to_string_lossy().into_owned(), moves);
-                }
-            }
-        }
-
-        Ok(games)
-    }
-
-    fn parse_games(sgfs: &mut HashMap<String, String>) -> HashMap<String, Vec<SgfNode<go::Prop>>> {
-        let mut games: HashMap<String, Vec<SgfNode<go::Prop>>> = HashMap::new();
-
-        for (path, sgf) in sgfs.into_iter() {
-            if let Ok(game) = go::parse(&sgf) {
-                games.insert(path.to_owned(), game);
-            }
-        }
-
-        games
-    }
-
-    #[test]
-    fn test_load_sgf() {
-        let games = load_sgfs().unwrap();
-        let point = Point { x: 3, y: 3 };
-        let color = Color::White;
-        for (path, moves) in games {
-            for move_ in moves {
-                if move_.point == point && move_.color == color {
-                    println!("{:?}", path);
-                    break;
-                }
-            }
-        }
-    }
-}
+//#[cfg(test)]
+//mod tests {
+//    use super::*;
+//
+//    fn load_sgfs() -> Result<HashMap<String, Vec<Move>>, Box<dyn std::error::Error>> {
+//        let mut sgf_folder = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+//        sgf_folder.push("badukmovies-pro-collection");
+//
+//        let mut games = HashMap::new();
+//
+//        for entry in jwalk::WalkDir::new(sgf_folder) {
+//            let path = entry?.path();
+//            if path.extension().map_or(false, |ext| ext == "sgf") {
+//                let file_data = std::fs::read_to_string(path.clone())?;
+//                if let Ok(game) = go::parse(&file_data) {
+//                    let mut moves = Vec::new();
+//                    for node in sgf_traversal::variation_nodes(&game[0], 0).unwrap() {
+//                        for props in &node.sgf_node.properties {
+//                            match props {
+//                                go::Prop::W(go::Move::Move(point)) => {
+//                                    moves.push(Move {
+//                                        color: Color::White,
+//                                        point: point.clone(),
+//                                    });
+//                                    break;
+//                                }
+//                                go::Prop::B(go::Move::Move(point)) => {
+//                                    moves.push(Move {
+//                                        color: Color::Black,
+//                                        point: point.clone(),
+//                                    });
+//                                    break;
+//                                }
+//                                _ => {}
+//                            }
+//                        }
+//                    }
+//
+//                    games.insert(path.to_string_lossy().into_owned(), moves);
+//                }
+//            }
+//        }
+//
+//        Ok(games)
+//    }
+//
+//    //#[test]
+//    //fn test_load_sgf() {
+//    //    let games = load_sgfs().unwrap();
+//    //    let mut buf = Vec::new();
+//    //    games.serialize(&mut Serializer::new(&mut buf)).unwrap();
+//    //    std::fs::write("games.pack", buf).unwrap();
+//    //    //let point = Point { x: 3, y: 3 };
+//    //    //let color = Color::White;
+//    //    //for (path, moves) in games {
+//    //    //    for move_ in moves {
+//    //    //        if move_.point == point && move_.color == color {
+//    //    //            println!("{:?}", path);
+//    //    //            break;
+//    //    //        }
+//    //    //    }
+//    //    //}
+//    //}
+//    #[test]
+//    fn test_load_pack() {
+//        let data = include_bytes!("games.pack");
+//        let mut de = Deserializer::new(&data[..]);
+//        let games: HashMap<String, Vec<Move>> = Deserialize::deserialize(&mut de).unwrap();
+//        assert!(!games.is_empty());
+//        let point = Point { x: 3, y: 3 };
+//        let color = Color::White;
+//        for (path, moves) in games {
+//            for move_ in moves {
+//                if move_.point == point && move_.color == color {
+//                    println!("{:?}", path);
+//                    break;
+//                }
+//            }
+//        }
+//    }
+//}
