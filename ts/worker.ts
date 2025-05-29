5cabe3643a1d5b4753845397aa0c6c8833c76942
Kaspar Emanuel <kaspar@kitspace.org>
2025-05-29 15:15:54 +0100

    Implement basic queue in worker

--- ts/worker.ts
+++ ts/worker.ts
@@ -6,15 +6,30 @@ await initWasm();
 
 const wasmSearch = new WasmSearch();
 
-onmessage = async (e) => {
-  console.log("Worker: Message received from main script");
+let queue: Uint8Array[] = [];
 
+onmessage = (e) => {
   const { type, payload } = e.data;
-
   if (type === "search") {
-    const resultsBuf = await wasmSearch.search(payload);
-    postMessage({ type: "result", payload: resultsBuf }, [resultsBuf.buffer]);
+    queue.push(payload);
   }
 };
+
+setInterval(async () => {
+  if (queue.length > 0) {
+    // take the latest query and discard the rest
+    const query = queue.pop()!!;
+    queue = [];
+    const resultsBuf = await wasmSearch.search(query);
+    // give the JS event loop a chance to fill the queue
+    await new Promise((resolve) => setTimeout(resolve, 0));
+    // if there are no new queries, send this result
+    if (queue.length === 0) {
+      self.postMessage({ type: "result", payload: resultsBuf }, [
+        resultsBuf.buffer,
+      ]);
+    }
+  }
+}, 100);
 
 console.log("Worker: wasm initialized");
