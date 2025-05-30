8fea692756805d5a47ad0056e9b5e049203ac9c6
Kaspar Emanuel <kaspar@kitspace.org>
2025-05-30 22:53:44 +0100

    Add vite-plugin for typescript checking

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,19 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
+import checker from "vite-plugin-checker";
 
 export default defineConfig({
   optimizeDeps: {
     exclude: ["wasm-search"],
   },
-  plugins: [react(), tailwindcss(), wasm(), topLevelAwait()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    wasm(),
+    topLevelAwait(),
+    checker({ typescript: true}),
+  ],
   worker: {
     format: "es",
     plugins: () => [wasm(), topLevelAwait()],
