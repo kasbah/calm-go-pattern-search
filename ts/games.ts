6edf4975384cfa1b47c6eebf7a98f2af86c3af0b
Kaspar Emanuel <kaspar@kitspace.org>
2025-05-30 02:10:00 +0100

    Add all_empty_correctly_within to result scoring

--- ts/games.ts
+++ ts/games.ts
@@ -5,4 +5,5 @@ export type Game = {
   rotation: number; // 0: no rotation, 1-3: rotation index
   is_inverted: boolean; // whether the pattern colors were inverted
   is_mirrored: boolean; // whether the pattern was mirrored
+  all_empty_correctly_within: number; // distance from moves where all surrounding points are correctly empty
 };
