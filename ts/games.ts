1b6ea3c9439bf7cc54f8c52a38fdfd536999a944
Kaspar Emanuel <kaspar@kitspace.org>
2025-05-30 03:43:38 +0100

    Add moves to search results

--- ts/games.ts
+++ ts/games.ts
@@ -1,3 +1,8 @@
+export type Placement = {
+  color: 0 | 1;
+  point: { x: number; y: number };
+};
+
 export type Game = {
   path: string;
   score: number;
@@ -6,4 +11,5 @@ export type Game = {
   is_inverted: boolean; // whether the pattern colors were inverted
   is_mirrored: boolean; // whether the pattern was mirrored
   all_empty_correctly_within: number; // distance from moves where all surrounding points are correctly empty
+  moves: Array<Placement>;
 };
