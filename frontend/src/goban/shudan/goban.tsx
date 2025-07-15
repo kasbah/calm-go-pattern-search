c02c5a2a5849ee5c3e63c296d2078cc7f3a8e5a9
Kaspar Emanuel <kaspar@kitspace.org>
2025-07-15 14:03:46 +0100

    Switch shudan goban to a function component

--- frontend/src/goban/shudan/goban.tsx
+++ frontend/src/goban/shudan/goban.tsx
@@ -1,6 +1,5 @@
-import React, { Component, memo } from "react";
+import React, { memo, useMemo } from "react";
 import classnames from "classnames";
-import { produce } from "immer";
 
 import {
   vertexEquals,
@@ -89,41 +88,43 @@ export interface GobanProps {
   onVertexPointerLeave?: (evt: React.PointerEvent, vertex: Vertex) => void;
 }
 
-interface GobanState {
-  signMap: Map<0 | 1 | -1>;
-  width: number;
-  height: number;
-  rangeX: [number, number];
-  rangeY: [number, number];
+const GobanComponent: React.FC<GobanProps> = (props) => {
+  const {
+    innerProps = {},
+    vertexSize = 24,
+    coordX,
+    coordY,
+    busy,
+    signMap = [],
+    paintMap,
+    heatMap,
+    markerMap,
+    ghostStoneMap,
+    showCoordinates = false,
+    lines = [],
+    selectedVertices = [],
+    dimmedVertices = [],
+    rangeX = [0, Infinity],
+    rangeY = [0, Infinity],
+  } = props;
 
-  xs: number[];
-  ys: number[];
-  hoshis: Vertex[];
-}
+  // Calculate derived state from props
+  const { width, height, xs, ys, hoshis } = useMemo(() => {
+    const width = signMap.length === 0 ? 0 : signMap[0].length;
+    const height = signMap.length;
+    const xs = range(width).slice(rangeX[0], rangeX[1] + 1);
+    const ys = range(height).slice(rangeY[0], rangeY[1] + 1);
+    const hoshis = getHoshis(width, height);
 
-class GobanComponent extends Component<GobanProps, GobanState> {
-  constructor(props: GobanProps) {
-    super(props);
-
-    this.state = {
-      signMap: [],
-      width: 0,
-      height: 0,
-      rangeX: [0, Infinity],
-      rangeY: [0, Infinity],
-
-      xs: [],
-      ys: [],
-      hoshis: [],
-    };
-  }
+    return { width, height, xs, ys, hoshis };
+  }, [signMap, rangeX, rangeY]);
 
   // Memoize vertex event handlers to prevent recreation
-  createVertexEventHandlers = (eventHandlers: GobanProps) => {
+  const eventHandlers = useMemo(() => {
     return vertexEvents.reduce(
       (acc, eventName) => {
         const handlerName = `onVertex${eventName}` as keyof GobanProps;
-        const handler = eventHandlers[handlerName];
+        const handler = props[handlerName];
         if (handler) {
           acc[`on${eventName}`] = handler;
         }
@@ -131,266 +132,186 @@ class GobanComponent extends Component<GobanProps, GobanState> {
       },
       {} as Record<string, unknown>,
     );
-  };
+  }, [props]);
 
   // Memoize vertex creation to prevent unnecessary re-renders
-  createVertex = (
-    x: number,
-    y: number,
-    props: {
-      signMap?: Map<0 | 1 | -1>;
-      paintMap?: Map<0 | 1 | -1>;
-      heatMap?: Map<HeatVertex | null>;
-      markerMap?: Map<Marker | null>;
-      ghostStoneMap?: Map<GhostStone | null>;
-      selectedVertices: Vertex[];
-      dimmedVertices: Vertex[];
-    },
-  ) => {
-    const {
-      signMap,
-      paintMap,
-      heatMap,
-      markerMap,
-      ghostStoneMap,
-      selectedVertices,
-      dimmedVertices,
-    } = props;
+  const createVertex = useMemo(() => {
+    return (x: number, y: number) => {
+      const equalsVertex = (v: Vertex) => vertexEquals(v, [x, y]);
+      const selected = selectedVertices.some(equalsVertex);
 
-    const equalsVertex = (v: Vertex) => vertexEquals(v, [x, y]);
-    const selected = selectedVertices.some(equalsVertex);
+      return {
+        key: [x, y].join("-"),
+        position: [x, y] as Vertex,
 
-    return {
-      key: [x, y].join("-"),
-      position: [x, y] as Vertex,
+        sign: signMap?.[y]?.[x],
 
-      sign: signMap?.[y]?.[x],
+        heat: heatMap?.[y]?.[x],
+        marker: markerMap?.[y]?.[x],
+        ghostStone: ghostStoneMap?.[y]?.[x],
+        dimmed: dimmedVertices.some(equalsVertex),
 
-      heat: heatMap?.[y]?.[x],
-      marker: markerMap?.[y]?.[x],
-      ghostStone: ghostStoneMap?.[y]?.[x],
-      dimmed: dimmedVertices.some(equalsVertex),
+        paint: paintMap?.[y]?.[x],
+        paintLeft: paintMap?.[y]?.[x - 1],
+        paintRight: paintMap?.[y]?.[x + 1],
+        paintTop: paintMap?.[y - 1]?.[x],
+        paintBottom: paintMap?.[y + 1]?.[x],
+        paintTopLeft: paintMap?.[y - 1]?.[x - 1],
+        paintTopRight: paintMap?.[y - 1]?.[x + 1],
+        paintBottomLeft: paintMap?.[y + 1]?.[x - 1],
+        paintBottomRight: paintMap?.[y + 1]?.[x + 1],
 
-      paint: paintMap?.[y]?.[x],
-      paintLeft: paintMap?.[y]?.[x - 1],
-      paintRight: paintMap?.[y]?.[x + 1],
-      paintTop: paintMap?.[y - 1]?.[x],
-      paintBottom: paintMap?.[y + 1]?.[x],
-      paintTopLeft: paintMap?.[y - 1]?.[x - 1],
-      paintTopRight: paintMap?.[y - 1]?.[x + 1],
-      paintBottomLeft: paintMap?.[y + 1]?.[x - 1],
-      paintBottomRight: paintMap?.[y + 1]?.[x + 1],
-
-      selected,
-      selectedLeft:
-        selected && selectedVertices.some((v) => vertexEquals(v, [x - 1, y])),
-      selectedRight:
-        selected && selectedVertices.some((v) => vertexEquals(v, [x + 1, y])),
-      selectedTop:
-        selected && selectedVertices.some((v) => vertexEquals(v, [x, y - 1])),
-      selectedBottom:
-        selected && selectedVertices.some((v) => vertexEquals(v, [x, y + 1])),
+        selected,
+        selectedLeft:
+          selected && selectedVertices.some((v) => vertexEquals(v, [x - 1, y])),
+        selectedRight:
+          selected && selectedVertices.some((v) => vertexEquals(v, [x + 1, y])),
+        selectedTop:
+          selected && selectedVertices.some((v) => vertexEquals(v, [x, y - 1])),
+        selectedBottom:
+          selected && selectedVertices.some((v) => vertexEquals(v, [x, y + 1])),
+      };
     };
-  };
+  }, [
+    signMap,
+    paintMap,
+    heatMap,
+    markerMap,
+    ghostStoneMap,
+    selectedVertices,
+    dimmedVertices,
+  ]);
 
-  render() {
-    const { width, height, rangeX, rangeY, xs, ys, hoshis } = this.state;
-
-    const {
-      innerProps = {},
-      vertexSize = 24,
-      coordX,
-      coordY,
-      busy,
-      signMap,
-      paintMap,
-      heatMap,
-      markerMap,
-      ghostStoneMap,
-      showCoordinates = false,
-      lines = [],
-      selectedVertices = [],
-      dimmedVertices = [],
-    } = this.props;
-
-    // Memoize event handlers
-    const eventHandlers = this.createVertexEventHandlers(this.props);
-
-    // Memoize container style
-    const containerStyle: React.CSSProperties = {
+  // Memoize container style
+  const containerStyle: React.CSSProperties = useMemo(
+    () => ({
       display: "inline-grid",
       gridTemplateRows: showCoordinates ? "1em 1fr 1em" : "1fr",
       gridTemplateColumns: showCoordinates ? "1em 1fr 1em" : "1fr",
       fontSize: vertexSize,
       lineHeight: "1em",
-      ...(this.props.style ?? {}),
-    };
+      ...(props.style ?? {}),
+    }),
+    [showCoordinates, vertexSize, props.style],
+  );
 
-    // Memoize content style
-    const currentContentStyle: React.CSSProperties = {
+  // Memoize content style
+  const currentContentStyle: React.CSSProperties = useMemo(
+    () => ({
       ...contentStyle,
       width: `${xs.length}em`,
       height: `${ys.length}em`,
       gridRow: showCoordinates ? "2" : "1",
       gridColumn: showCoordinates ? "2" : "1",
-    };
+    }),
+    [xs.length, ys.length, showCoordinates],
+  );
 
-    // Memoize vertices style
-    const currentVerticesStyle: React.CSSProperties = {
+  // Memoize vertices style
+  const currentVerticesStyle: React.CSSProperties = useMemo(
+    () => ({
       ...verticesStyle,
       display: "grid",
       gridTemplateColumns: `repeat(${xs.length}, 1em)`,
       gridTemplateRows: `repeat(${ys.length}, 1em)`,
-    };
+    }),
+    [xs.length, ys.length],
+  );
 
-    const createVertexProps = {
-      signMap,
-      paintMap,
-      heatMap,
-      markerMap,
-      ghostStoneMap,
-      selectedVertices,
-      dimmedVertices,
-    };
+  return (
+    <div
+      {...innerProps}
+      id={props.id}
+      className={classnames(
+        "shudan-goban",
+        "shudan-goban-image",
+        {
+          "shudan-busy": busy,
+          "shudan-coordinates": showCoordinates,
+        },
+        props.class ?? props.className,
+      )}
+      style={containerStyle}
+    >
+      {showCoordinates && (
+        <CoordX
+          xs={xs}
+          style={{ gridRow: "1", gridColumn: "2" }}
+          coordX={coordX}
+        />
+      )}
+      {showCoordinates && (
+        <CoordY
+          height={height}
+          ys={ys}
+          style={{ gridRow: "2", gridColumn: "1" }}
+          coordY={coordY}
+        />
+      )}
 
-    return (
-      <div
-        {...innerProps}
-        id={this.props.id}
-        className={classnames(
-          "shudan-goban",
-          "shudan-goban-image",
-          {
-            "shudan-busy": busy,
-            "shudan-coordinates": showCoordinates,
-          },
-          this.props.class ?? this.props.className,
-        )}
-        style={containerStyle}
-      >
-        {showCoordinates && (
-          <CoordX
-            xs={xs}
-            style={{ gridRow: "1", gridColumn: "2" }}
-            coordX={coordX}
-          />
-        )}
-        {showCoordinates && (
-          <CoordY
-            height={height}
-            ys={ys}
-            style={{ gridRow: "2", gridColumn: "1" }}
-            coordY={coordY}
-          />
-        )}
+      <div className="shudan-content" style={currentContentStyle}>
+        <Grid
+          vertexSize={vertexSize}
+          width={width}
+          height={height}
+          xs={xs}
+          ys={ys}
+          hoshis={hoshis}
+        />
 
-        <div className="shudan-content" style={currentContentStyle}>
-          <Grid
-            vertexSize={vertexSize}
-            width={width}
-            height={height}
-            xs={xs}
-            ys={ys}
-            hoshis={hoshis}
-          />
-
-          <div className="shudan-vertices" style={currentVerticesStyle}>
-            {ys.map((y) =>
-              xs.map((x) => {
-                const vertexData = this.createVertex(x, y, createVertexProps);
-                const { key, ...restVertexProps } = vertexData;
-                return (
-                  <VertexComponent
-                    key={key}
-                    {...restVertexProps}
-                    {...eventHandlers}
-                  />
-                );
-              }),
-            )}
-          </div>
-
-          <svg className="shudan-lines" style={linesStyle}>
-            <g
-              transform={`translate(-${rangeX[0] * vertexSize} -${
-                rangeY[0] * vertexSize
-              })`}
-            >
-              {lines.map(({ v1, v2, type }, i) => (
-                <Line
-                  key={i}
-                  v1={v1}
-                  v2={v2}
-                  type={type}
-                  vertexSize={vertexSize}
+        <div className="shudan-vertices" style={currentVerticesStyle}>
+          {ys.map((y) =>
+            xs.map((x) => {
+              const vertexData = createVertex(x, y);
+              const { key, ...restVertexProps } = vertexData;
+              return (
+                <VertexComponent
+                  key={key}
+                  {...restVertexProps}
+                  {...eventHandlers}
                 />
-              ))}
-            </g>
-          </svg>
+              );
+            }),
+          )}
         </div>
 
-        {showCoordinates && (
-          <CoordY
-            height={height}
-            ys={ys}
-            style={{ gridRow: "2", gridColumn: "3" }}
-            coordY={coordY}
-          />
-        )}
-        {showCoordinates && (
-          <CoordX
-            xs={xs}
-            style={{ gridRow: "3", gridColumn: "2" }}
-            coordX={coordX}
-          />
-        )}
+        <svg className="shudan-lines" style={linesStyle}>
+          <g
+            transform={`translate(-${rangeX[0] * vertexSize} -${
+              rangeY[0] * vertexSize
+            })`}
+          >
+            {lines.map(({ v1, v2, type }, i) => (
+              <Line
+                key={i}
+                v1={v1}
+                v2={v2}
+                type={type}
+                vertexSize={vertexSize}
+              />
+            ))}
+          </g>
+        </svg>
       </div>
-    );
-  }
 
-  static getDerivedStateFromProps(
-    props: GobanProps,
-    state: GobanState,
-  ): Partial<GobanState> | null {
-    const {
-      signMap = [],
-      rangeX = [0, Infinity],
-      rangeY = [0, Infinity],
-    } = props;
-
-    const width = signMap.length === 0 ? 0 : signMap[0].length;
-    const height = signMap.length;
-
-    if (state.width === width && state.height === height) {
-      return produce(state, (draft) => {
-        draft.signMap = signMap;
-
-        if (
-          !vertexEquals(state.rangeX as Vertex, rangeX as Vertex) ||
-          !vertexEquals(state.rangeY as Vertex, rangeY as Vertex)
-        ) {
-          // Range changed
-          draft.rangeX = rangeX;
-          draft.rangeY = rangeY;
-          draft.xs = range(width).slice(rangeX[0], rangeX[1] + 1);
-          draft.ys = range(height).slice(rangeY[0], rangeY[1] + 1);
-        }
-      });
-    }
-
-    // Board size changed
-    return produce({} as GobanState, (draft) => {
-      draft.signMap = signMap;
-      draft.width = width;
-      draft.height = height;
-      draft.rangeX = rangeX;
-      draft.rangeY = rangeY;
-      draft.xs = range(width).slice(rangeX[0], rangeX[1] + 1);
-      draft.ys = range(height).slice(rangeY[0], rangeY[1] + 1);
-      draft.hoshis = getHoshis(width, height);
-    });
-  }
-}
+      {showCoordinates && (
+        <CoordY
+          height={height}
+          ys={ys}
+          style={{ gridRow: "2", gridColumn: "3" }}
+          coordY={coordY}
+        />
+      )}
+      {showCoordinates && (
+        <CoordX
+          xs={xs}
+          style={{ gridRow: "3", gridColumn: "2" }}
+          coordX={coordX}
+        />
+      )}
+    </div>
+  );
+};
 
 // Simple shallow equality check
 const shallowEquals = (a: unknown, b: unknown): boolean => {
